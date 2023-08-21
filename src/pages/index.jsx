12888d18a4d9725904d691fc4c82aa00eaaa0c74
TheoDosSantos <theo@callbruno.com>
2023-08-21 11:26:17 +0200

    Update - Global - Setup starter

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import styles from "@/styles/pages/home.module.scss";
+
+const Home = () => {
+    return (
+        <section className={styles.section_start}>
+            <h1 className={styles.title}>Welcome to my blog</h1>
+        </section>
+    )
+}
+
+export default Home
