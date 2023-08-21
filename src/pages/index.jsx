5ea14081cc4108b448f41f000db6ed1cfafe3477
TheoDosSantos <theo@callbruno.com>
2023-08-21 15:13:05 +0200

    Update - Home - Starting desktop integration

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,37 @@
 import styles from "@/styles/pages/home.module.scss";
 
 const Home = () => {
-    return (
-        <section className={styles.section_start}>
-            <h1 className={styles.title}>Welcome to my blog</h1>
-        </section>
-    )
-}
+  return (
+    <section className={styles.section_start}>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div className={styles.container_title}>
+            <h1 className={styles.title}>
+              TOURNOIS
+              <br />
+              SSBU <span>BRUNO</span>
+            </h1>
+            <p className={styles.text}>
+              Application web permettant d'aider à l'organisation de tournois
+              SSBU (Super Smash Bros. Ultimate).<br></br>
 
-export default Home
+            </p>
+            <ul>
+              <li>Inscription des participants</li>
+              <li>Création des poules</li>
+              <li>Gestion des matchs, scores & phases finales</li>
+              <li>Affichage du classement</li>
+            </ul>
+          </div>
+          <div className={styles.container_btn}>
+            <div className={styles.btn}>
+              <span className={styles.btn_text}>Commencer</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
